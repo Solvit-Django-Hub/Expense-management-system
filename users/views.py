46cde6bbebefe9e318d1894806a1e529036b662b
Cynthy Niganze <cynthynigaira@gmail.com>
2026-09-11 14:33:11 +0200

    creating business logic

--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
+from django.http import request
 from django.shortcuts import render
+from .models import User
+from users.forms import ProfileForm
+from users.serializers import Serializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(["GET", "POST"])
+def register_user(request):
+
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = Serializer(users, many=True)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    serializer = Serializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+    )
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "fullname": request.user.full_name,
+            "email": request.user.email,
+            })
+
+    
